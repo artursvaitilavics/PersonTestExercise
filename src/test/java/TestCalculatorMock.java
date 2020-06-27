d4fdceeb44ff39b4bf62321a83f5465d1f7268a0
artursvaitilavics <artyvt@gmail.com>
2020-06-27 14:26:20 +0300

    mockit pabeigts

--- src/test/java/TestCalculatorMock.java
+++ src/test/java/TestCalculatorMock.java
@@ -8,19 +8,30 @@ import org.junit.*;
 
 public class TestCalculatorMock {
 
+//    @Test
+//    public void testMockedCalculator() {
+//        Calculator calc = mock(Calculator.class);
+//        when(calc.add(eq(1), eq(3))).thenReturn(123);
+//        when(calc.add(eq(1), eq(4))).thenReturn(5);
+//
+//        when(calc.add(anyInt(), anyInt())).thenReturn(55);
+//
+//        assert calc.add(1, 3) == 123;
+//        assertEquals(5, calc.add(1, 4));
+//        assertEquals(55, calc.add(1, 4));
+//
+//        verify(calc, times(3)).add(anyInt(),anyInt());
+//    }
+
     @Test
-    public void testMockedCalculator() {
-        Calculator calc = mock(Calculator.class);
-        when(calc.add(eq(1), eq(3))).thenReturn(123);
-        when(calc.add(eq(1), eq(4))).thenReturn(5);
+    public void testSpiesCalculator(){
+        Calculator calculator = new Calculator();
+        Calculator spiedCalculator = spy(calculator);
 
-        when(calc.add(anyInt(), anyInt())).thenReturn(55);
+        when(spiedCalculator.multiply(eq(3), eq(5))).thenReturn(16);
 
-        assert calc.add(1, 3) == 123;
-        assertEquals(5, calc.add(1, 4));
-
-        assertEquals(55, calc.add(1, 4));
-
-        verify(calc, times(3)).add(anyInt(),anyInt());
+        assertEquals(16,spiedCalculator.multiply(3,5)); // nem no fake calclulatora
+        assertEquals(18,spiedCalculator.multiply(3,6)); // vins nem no iistaa calclulatora
     }
+
 }
