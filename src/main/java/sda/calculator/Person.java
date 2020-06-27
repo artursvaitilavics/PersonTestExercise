30f16336cc745732adf743a31478249d84695e99
artursvaitilavics <artyvt@gmail.com>
2020-06-27 15:24:10 +0300

    Person Extended Exercise

--- src/main/java/sda/calculator/Person.java
+++ src/main/java/sda/calculator/Person.java
@@ -3,11 +3,38 @@ package sda.calculator;
 public class Person {
     int age;
 
-    public Person(int age){
+    public Person(int age) {
         this.age = age;
     }
-    public boolean isTeenager(){
-        if(age > 12 && age < 20){
+
+    public boolean isTeenager() {
+        if (age > 12 && age < 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isChild() {
+        if (age <= 12 && age >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isAdult() {
+        if (age >= 20 && age <= 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isGhost() {
+        if (!isChild()
+                && !isTeenager()
+                && !isAdult()) {
             return true;
         }else {
             return false;
--- src/test/java/TestPerson.java
+++ src/test/java/TestPerson.java
@@ -1,12 +1,51 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import sda.calculator.Person;
 
+@RunWith(Parameterized.class)
 public class TestPerson {
+    @Parameterized.Parameters
+    public static Collection<Integer[]> parameters() {
+        return Arrays.asList(new Integer[][]{
+                {15},
+                {55},
+                {25},
+                {8},
+                {115},
+                {17}
+        });
+    }
+
+    @Parameterized.Parameter(0)
+    public int age1;
+//    @Parameterized.Parameter(1)
+//    public int age2;
+
     @Test
     public void testIsTeenager() {
-        Person person001 = new Person(15);
+        Person person001 = new Person(age1);
         assert person001.isTeenager();
-        Person person002 = new Person(25);
-        assert !person002.isTeenager();
+    }
+    @Test
+    public void testIsChild() {
+        Person person001 = new Person(age1);
+        assert person001.isChild();
+    }
+    @Test
+    public void testIsAdult() {
+        Person person001 = new Person(age1);
+        assert person001.isAdult();
+    }
+    @Test
+    public void testIsDead() {
+        Person person001 = new Person(age1);
+        assert person001.isGhost();
     }
 }
